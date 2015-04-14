5c911b039d2e66a41b6b8617a87500cbf5f95c2b
ksattler <kai@Kai-Uwes-MacBook-Air.local>
2015-04-14 18:43:18 +0200

    Comments added

--- src/main/scala/dbis/pig/PigParser.scala
+++ src/main/scala/dbis/pig/PigParser.scala
@@ -6,10 +6,17 @@ import scala.util.parsing.combinator.JavaTokenParsers
  * Created by kai on 31.03.15.
  */
 
-
+/**
+ * A parser for the (extended) Pig language.
+ */
 class PigParser extends JavaTokenParsers {
   override protected val whiteSpace = """(\s|--.*)+""".r
 
+  /**
+   * A helper class for supporting case-insensitive keywords.
+   *
+   * @param str the keyword string to be handled.
+   */
   class CaseInsensitiveString(str: String) {
     def ignoreCase: Parser[String] = ("""(?i)\Q""" + str + """\E""").r
   }
@@ -23,10 +30,14 @@ class PigParser extends JavaTokenParsers {
   def bag: Parser[String] = ident
   def fileName: Parser[String] = stringLiteral ^^ {str => str.substring(1, str.length - 1)}
 
+  /*
+   * A reference can be a named field,a positional field (e.g $0, $1, ...) or a literal.
+   */
   def posField: Parser[Ref] = """\$[0-9]*""".r ^^ { p => PositionalField(p.substring(1, p.length).toInt) }
   def namedField: Parser[Ref] = ident ^^ { i => NamedField(i) }
   def literalField: Parser[Ref] = (floatingPointNumber ^^ { n => Value(n) } | stringLiteral ^^ { s => Value(s) })
   def ref: Parser[Ref] = ( posField | namedField | literalField )
+
   def predicate: Parser[Predicate] = ref ~ ("!=" | "<=" | ">=" | "==" | "<" | ">") ~ ref ^^ {
     case a ~ op ~ b => op match {
       case "==" => Eq(a, b)
@@ -38,6 +49,9 @@ class PigParser extends JavaTokenParsers {
     }
   }
 
+  /*
+   * The list of case-insensitive keywords we want to accept.
+   */
   lazy val loadKeyword = "load".ignoreCase
   lazy val dumpKeyword = "dump".ignoreCase
   lazy val storeKeyword = "store".ignoreCase
@@ -51,10 +65,16 @@ class PigParser extends JavaTokenParsers {
   lazy val describeKeyword = "describe".ignoreCase
   lazy val limitKeyword = "limit".ignoreCase
   lazy val usingKeyword = "using".ignoreCase
+  lazy val foreachKeyword = "foreach".ignoreCase
+  lazy val generateKeyword = "generate".ignoreCase
 
+  /*
+   * <A> = LOAD <B> "<FileName>" USING <StorageFunc> (<OptParameters>)
+   */
   def usingClause: Parser[(String, List[String])] = usingKeyword ~ ident ~ "(" ~ repsep(stringLiteral, ",") ~ ")" ^^ {
     case _ ~ loader ~ _ ~ params ~ _ => (loader, params)
   }
+
   def loadStmt: Parser[PigOperator] = bag ~ "=" ~ loadKeyword ~ fileName ~ (usingClause?) ^^ {
     case b ~ _ ~ _ ~ f ~ u => u match {
       case Some(p) => Load(b, f, p._1, if (p._2.isEmpty) null else p._2)
@@ -62,29 +82,60 @@ class PigParser extends JavaTokenParsers {
     }
   }
 
+  /*
+   * DUMP <A>
+   */
   def dumpStmt: Parser[PigOperator] = dumpKeyword ~ bag ^^ { case _ ~ b => Dump(b) }
 
+  /*
+   * STORE <A> INTO "<FileName>"
+   */
   def storeStmt: Parser[PigOperator] = storeKeyword ~ bag ~ intoKeyword ~ fileName ^^ { case _ ~ b ~  _ ~ f => Store(b, f) }
 
-  def foreachStmt: Parser[PigOperator] = bag ~ "=" ~ "foreach" ~ bag ~ "generate" ~ exprList ^^ {
+  /*
+   * <A> = FOREACH <B> GENERATE ....
+   */
+  def foreachStmt: Parser[PigOperator] = bag ~ "=" ~ foreachKeyword ~ bag ~ generateKeyword ~ exprList ^^ {
     case out ~ _ ~ _ ~ in ~ _ ~ ex => Foreach(out, in, ex)
   }
 
+  /*
+   * <A> = FILTER <B> BY <Predicate>
+   */
   def filterStmt: Parser[PigOperator] = bag ~ "=" ~ filterKeyword ~ bag ~ byKeyword ~ predicate ^^ {
     case out ~ _ ~ _ ~ in ~ _ ~ pred => Filter(out, in, pred)
   }
+
+  /*
+   * DESCRIBE <A>
+   */
   def describeStmt: Parser[PigOperator] = describeKeyword ~ bag ^^ { case _ ~ b => Describe(b) }
 
+  /*
+   * <A> = GROUP <B> ALL
+   * <A> = GROUP <B> BY <Ref>
+   * <A> = GROUP <B> B> ( <ListOfRefs> )
+   */
   def refList: Parser[List[Ref]] = (ref ^^ { r => List(r) } | "(" ~ repsep(ref, ",") ~ ")" ^^ { case _ ~ rlist ~ _ => rlist})
   def groupingClause: Parser[GroupingExpression] = allKeyword ^^ { s => GroupingExpression(List())} |
     (byKeyword ~ refList ^^ { case _ ~ rlist => GroupingExpression(rlist)})
   def groupingStmt: Parser[PigOperator] = bag ~ "=" ~ groupKeyword ~ bag ~ groupingClause ^^ {
     case out ~ _ ~ _ ~ in ~ grouping => Grouping(out, in, grouping) }
 
+  /*
+   * <A> = DISTINCT <B>
+   */
   def distinctStmt: Parser[PigOperator] = bag ~ "=" ~ distinctKeyword ~ bag ^^ { case out ~ _ ~ _ ~ in => Distinct(out, in) }
 
+  /*
+   * <A> = LIMIT <B> <Num>
+   */
   def limitStmt: Parser[PigOperator] = bag ~ "=" ~ limitKeyword ~ bag ~ num ^^ { case out ~ _ ~ _ ~ in ~ num => Limit(out, in, num) }
 
+  /*
+   * <A> = JOIN <B> BY <Ref>, <C> BY <Ref>, ...
+   * <A> = JOIN <B> BY ( <ListOfRefs> ), <C> BY ( <ListOfRefs>), ...
+   */
   def joinExpr: Parser[(String, List[Ref])] = bag ~ byKeyword ~ refList ^^ { case b ~ _ ~ rlist => (b, rlist) }
   def joinExprList: Parser[List[(String, List[Ref])]] = repsep(joinExpr, ",") ^^ { case jlist => jlist }
   def extractJoinRelation(jList: List[(String, List[Ref])]): List[String] = { jList.map{ case (alias, refs) => alias } }
@@ -92,8 +143,15 @@ class PigParser extends JavaTokenParsers {
   def joinStmt: Parser[PigOperator] = bag ~ "=" ~ joinKeyword ~ joinExprList ^^ {
     case out ~ _ ~ _ ~ jlist => Join(out, extractJoinRelation(jlist), extractJoinFields(jlist)) }
 
+  /*
+   * A statement cocanuld be one of the above delimited by a semicolon.
+   */
   def stmt: Parser[PigOperator] = (loadStmt | dumpStmt | describeStmt | foreachStmt | filterStmt | groupingStmt |
     distinctStmt | joinStmt | storeStmt | limitStmt) ~ ";" ^^ {
     case op ~ _  => op }
+
+  /*
+   * A script is a list of statements.
+   */
   def script: Parser[List[PigOperator]] = rep(stmt)
 }
