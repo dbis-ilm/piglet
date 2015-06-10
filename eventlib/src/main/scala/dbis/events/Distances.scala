3fec8319802106065da88effa9be24044c95d398
ksattler <kai@wl-r1-81.rz.tu-ilmenau.de>
2015-06-10 08:49:23 +0200

    Minor cleanup in build.sbt

--- eventlib/src/main/scala/dbis/events/Distances.scala
+++
@@ -1,9 +0,0 @@
-package dbis.events
-
-/**
- * Created by kai on 01.06.15.
- */
-object Distances {
-  def spatialDistance(longitude: Double, latitude: Double, refLongitude: Double, refLatitude: Double): Double = 1.0
-  def temporalDistance(startTime: Long, endTime: Long, refTime: Long): Long = 1
-}
