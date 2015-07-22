dd571ba5889257d2ce40178f32d76c649474f4f1
Stefan Hagedorn <stefan.hagedorn@tu-ilmenau.de>
2015-07-22 16:15:21 +0200

    fixed imports in spark template

--- src/main/scala/dbis/pig/backends/BackendManager.scala
+++ src/main/scala/dbis/pig/backends/BackendManager.scala
@@ -1,15 +1,18 @@
 package dbis.pig.backends
 
 import dbis.pig.tools.Conf
+import com.typesafe.scalalogging.LazyLogging
 
 
 /**
  * @author hage
  */
-object BackendManager {
+object BackendManager extends LazyLogging {
  
   def backendConf(backend: String): PigletBackend = {
     val className = Conf.backendConf(backend)
+    
+    logger.debug(s"loading runner class with name: $className")
     
     Class.forName(className).newInstance().asInstanceOf[PigletBackend]
   }
