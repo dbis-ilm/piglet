007cc44bdd973902b59f50bef5c2bdb0a83060d3
Stefan Hagedorn <stefan.hagedorn@tu-ilmenau.de>
2015-07-31 10:23:02 +0200

    export config file to $USER_HOME/.piglet/
    The config file may contain values that should be editable by the users. However, if we ship our application as a jar, editing the file is no longer (easily) possible.
    Thus, on startup we check if $USER_HOME/.piglet/application.conf exists. If not, we copy the file to this location and load it from there. There is also a new cmdline switch (u, update-config) to force copying the shipped file to the user's home.

--- src/main/scala/dbis/pig/tools/Conf.scala
+++ src/main/scala/dbis/pig/tools/Conf.scala
@@ -2,11 +2,60 @@ package dbis.pig.tools
 
 import com.typesafe.config.ConfigFactory
 import java.io.File
+import java.nio.file.Paths
+import java.nio.file.Files
+import java.nio.file.LinkOption
+import java.nio.file.CopyOption
+import com.typesafe.scalalogging.LazyLogging
+import com.typesafe.config.Config
+import java.nio.file.StandardCopyOption
 
-object Conf {
+/**
+ * This is the global configuration object that contains all user-defined values
+ */
+object Conf extends LazyLogging {
   
-  // loads the default configuration file in resources/application.conf
-  private val appconf = ConfigFactory.load()
+  /**
+   * The path to the config file. It will resolve to $USER_HOME/.piglet/application.conf
+   */
+  private val configFile = Paths.get(System.getProperty("user.home"), ".piglet", "application.conf")
+  
+  /**
+   * Load the configuration.
+   * 
+   * This loads the configuration from the user's home directory. If the config file cannot be found
+   * (see [[Conf#configFile]]) the default values found in src/main/resources/application.conf are
+   * copied to [[Conf#configFile]]
+   * 
+   * @return Returns the config object
+   */
+  private def loadConf: Config = {
+    
+    // 1. check if the config file in the user's home directory exists
+    if(!Files.exists(configFile)) {
+      
+      // 2. if not, create parent directory if necessary
+      if(!Files.exists(configFile.getParent)) {
+        Files.createDirectories(configFile.getParent)
+        logger.info(s"""created program directory at ${configFile.getParent}""")
+      }
+      
+      // 3. copy config file
+      copyConfigFile()
+    }
+  
+    // 4. parse the newly created config file
+    ConfigFactory.parseFile(configFile.toFile())
+  }
+
+  protected[pig] def copyConfigFile() = {
+    val source = Conf.getClass.getClassLoader.getResourceAsStream("application.conf")
+    Files.copy(source, configFile, StandardCopyOption.REPLACE_EXISTING)
+    logger.debug(s"copied config file to $configFile")
+  }
+  
+  // loads the configuration file 
+  private val appconf = loadConf
   
   
   def materializationBaseDir: File = new File(appconf.getString("materialization.basedir"))
