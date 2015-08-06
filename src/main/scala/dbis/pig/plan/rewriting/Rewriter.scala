87758357fa224055e38d5060547e2e6de50b5063
Wieland Hoffmann <wieland.hoffmann@tu-ilmenau.de>
2015-08-06 16:03:00 +0200

    Move all implemented rules to a Rules object
    Having helper functions and rules in the Rewriter object was getting a
    bit too much.

--- src/main/scala/dbis/pig/plan/rewriting/Rewriter.scala
+++ src/main/scala/dbis/pig/plan/rewriting/Rewriter.scala
@@ -19,6 +19,7 @@ package dbis.pig.plan.rewriting
 import dbis.pig.op.{And, BinaryExpr, Filter, Load, Materialize, OrderBy, PigOperator, Pipe, Store, _}
 import dbis.pig.plan.{DataflowPlan, MaterializationManager}
 import dbis.pig.tools.BreadthFirstBottomUpWalker
+import dbis.pig.plan.rewriting.Rules.registerAllRules
 import org.kiama.rewriting.Rewriter._
 import org.kiama.rewriting.Strategy
 import scala.collection.mutable
@@ -169,246 +170,6 @@ object Rewriter extends LazyLogging {
     newPlan
   }
 
-    /** Put Filters before Joins if we can figure out which side of the join contains the fields used in the Filters
-      * predicate
-      *
-      */
-  private def filterBeforeJoin(join: Join, filter: Filter): Option[Filter] = {
-    // Collect all Fields of the Filter operator
-    def extractFields(p: Expr): List[Ref] =
-      p match {
-        case RefExpr(Value(_)) => Nil
-        case RefExpr(r) => List(r)
-        case BinaryExpr(l, r) => extractFields(l) ++ extractFields(r)
-        case And(l, r) => extractFields(l) ++ extractFields(r)
-        case Or(l, r) => extractFields(l) ++ extractFields(r)
-        case Not(p) => extractFields(p)
-        case FlattenExpr(e) => extractFields(e)
-        case PExpr(e) => extractFields(e)
-        case CastExpr(t, e) => extractFields(e)
-        case MSign(e) => extractFields(e)
-        case Func(f, p) => p.flatMap(extractFields)
-        case ConstructBagExpr(ex) => ex.flatMap(extractFields)
-        case ConstructMapExpr(ex) => ex.flatMap(extractFields)
-        case ConstructTupleExpr(ex) => ex.flatMap(extractFields)
-      }
-
-    val fields = extractFields(filter.pred)
-
-    if (fields.exists(field => !field.isInstanceOf[NamedField])) {
-      // A field is not a NamedField, we don't know how to figure out where it's coming from
-      return None
-    }
-
-    val namedfields = fields.map(_.asInstanceOf[NamedField])
-
-    val inputs = join.inputs.map(_.producer)
-
-    var inputWithCorrectFields: Option[PigOperator] = None
-
-    inputs foreach { inp =>
-      namedfields foreach { f =>
-        if (inp.schema.isEmpty) {
-          // The schema of an input is not defined, abort because it might or might not contain one of the fields
-          // we're looking for
-          return None
-        }
-
-        if (inp.schema.get.fields.exists(_.name == f.name)) {
-          if (inputWithCorrectFields.isEmpty) {
-            // We found an input that has the correct field and we haven't found one with a matching field before
-            inputWithCorrectFields = Some(inp)
-          }
-
-          if (inputWithCorrectFields.get != inp) {
-            // We found an input that has a field we're looking for but we already found an input with a matching
-            // field but it's not the same as the current input.
-            // TODO Just abort the operation for now - in the future, we could try to split push different Filters into
-            // multiple inputs of the Join.
-            return None
-          }
-        }
-      }
-    }
-
-    if (inputWithCorrectFields.isEmpty) {
-      // We did not find an input that has all the fields we're looking for - this might be because the fields are
-      // spread over multiple input or because the fields do not actually exist - in any case, abort.
-      return None
-    }
-
-    // We found a single input that has all the fields used in the predicate, which means that we can safely put the
-    // Filter between that and the Join.
-    // We can't use fixInputsAndOutputs here because they don't work correctly for Joins
-    val inp = inputWithCorrectFields.get
-
-    // First, make the Filter a consumer of the correct input
-    inp.outputs foreach { outp =>
-      if (outp.consumer contains join) {
-        outp.consumer = outp.consumer.filterNot(_ == join) :+ filter
-      }
-    }
-
-    filter.inputs.filterNot(_.producer == join) :+ Pipe(inp.outPipeName, inp)
-
-    // Second, make the Filter an input of the Join
-    join.inputs = join.inputs.filterNot(_.name == inp.outPipeName) :+ Pipe(filter.outPipeName, filter)
-
-    // Third, replace the Filter in the Joins outputs with the Filters outputs
-    join.outputs foreach { outp =>
-      if (outp.consumer contains filter) {
-        outp.consumer = outp.consumer.filterNot(_ == filter) ++ filter.outputs.flatMap(_.consumer)
-      }
-    }
-
-    // Fourth, make the Join the producer of all the Filters outputs inputs
-    filter.outputs foreach { outp =>
-      outp.consumer foreach { cons =>
-        cons.inputs map { cinp =>
-          if (cinp.producer == filter) {
-            Pipe(join.outPipeName, join)
-          } else {
-            cinp
-          }
-        }
-      }
-    }
-    Some(filter)
-  }
-
-  /** Merges two [[dbis.pig.op.Filter]] operations if one is the only input of the other.
-    *
-    * @param parent The parent filter.
-    * @param child The child filter.
-    * @return On success, an Option containing a new [[dbis.pig.op.Filter]] operator with the predicates of both input
-    *         Filters, None otherwise.
-    */
-  private def mergeFilters(parent: Filter, child: Filter): Option[PigOperator] =
-    Some(Filter(child.out, parent.in, And(parent.pred, child.pred)))
-
-  private def splitIntoToFilters(node: Any): Option[List[PigOperator]] = node match {
-    case node@SplitInto(inPipeName, splits) =>
-      val filters = (for (branch <- splits) yield branch.output.name -> Filter(branch.output, inPipeName, branch
-        .expr)).toMap
-      node.inputs = node.inputs.map(p => {
-        p.consumer = p.consumer.filterNot(_ == node)
-        p
-      })
-      // For all outputs
-      node.outputs.iterator foreach (_.consumer.foreach(output => {
-        // Iterate over their inputs
-        output.inputs foreach (input => {
-          // Check if the relation name is one of the names our SplitBranches write
-          if (filters contains input.name) {
-            // Replace SplitInto with the appropriate Filter
-            output.inputs = output.inputs.filter(_.producer != node) :+ Pipe(input.name, filters(input.name))
-            filters(input.name).inputs = node.inputs
-          }
-        })
-      }
-      ))
-      Some(filters.values.toList)
-    case _ => None
-  }
-
-  /** Replaces sink nodes that are not [[dbis.pig.op.Store]] operators with [[dbis.pig.op.Empty]] ones.
-    *
-    * @param node
-    * @return
-    */
-  //noinspection ScalaDocMissingParameterDescription
-  private def removeNonStorageSinks(node: Any): Option[PigOperator] = node match {
-    // Store and Dump are ok
-    case Store(_, _, _) => None
-    case Dump(_) => None
-    // To prevent recursion, empty is ok as well
-    case Empty(_) => None
-    case op: PigOperator =>
-      op.outputs match {
-      case Pipe(_, _, Nil) :: Nil | Nil =>
-        val newNode = Empty(Pipe(""))
-        newNode.inputs = op.inputs
-        Some(newNode)
-      case _ => None
-    }
-    case _ => None
-  }
-
-  /** If an operator is followed by an Empty node, replace it with the Empty node
-    *
-    * @param parent
-    * @param child
-    * @return
-    */
-  //noinspection ScalaDocMissingParameterDescription
-  private def mergeWithEmpty(parent: PigOperator, child: Empty): Option[PigOperator] = Some(child)
-
-  /** Applies rewriting rule R1 of the paper "SPARQling Pig - Processing Linked Data with Pig latin".
-    *
-    * @param term
-    * @return Some Load operator, if `term` was an RDFLoad operator loading a remote resource
-    */
-  //noinspection ScalaDocMissingParameterDescription
-  private def R1(term: Any): Option[PigOperator] = term match {
-    case op@RDFLoad(p, uri, None) =>
-      if (uri.getScheme == "http" || uri.getScheme == "https") {
-        Some(Load(p, uri, op.schema, "pig.SPARQLLoader", List("SELECT * WHERE { ?s ?p ?o }")))
-      } else {
-        None
-      }
-    case _ => None
-  }
-
-  /** Applies rewriting rule R2 of the paper "SPARQling Pig - Processing Linked Data with Pig latin".
-    *
-    * @param parent
-    * @param child
-    * @return
-    */
-  //noinspection ScalaDocMissingParameterDescription
-  private def R2(parent: RDFLoad, child: BGPFilter): Option[PigOperator] = {
-    Some(Load(child.out, parent.uri, parent.schema, "pig.SPARQLLoader",
-    List(child.patterns.head.toString))
-    )
-  }
-
-  /** Applies rewriting rule L2 of the paper "SPARQling Pig - Processing Linked Data with Pig latin".
-    *
-    * @param term
-    * @return Some Load operator, if `term` was an RDFLoad operator loading a resource from hdfs
-    */
-  //noinspection ScalaDocMissingParameterDescription
-  private def L2(term: Any): Option[PigOperator] = term match {
-    case op@RDFLoad(p, uri, grouped : Some[String]) =>
-      if (uri.getScheme == "hdfs") {
-        Some(Load(p, uri, op.schema, "BinStorage"))
-      } else {
-        None
-      }
-    case _ => None
-  }
-
-  /** Applies rewriting rule F1 of the paper "SPARQling Pig - Processing Linked Data with Pig latin".
-   *
-   * @return A strategy that removes BGPFilters that use only unbound variables in their single pattern
-   */
-  private def F1 = rulefs[BGPFilter] {
-    case op@BGPFilter(_, _, patterns) =>
-      if (patterns.length > 1 || patterns.isEmpty) {
-        fail
-      } else {
-        val pattern = patterns.head
-        if (!pattern.subj.isInstanceOf[Value]
-          && !pattern.pred.isInstanceOf[Value]
-          && !pattern.obj.isInstanceOf[Value]) {
-          removalStrategy(op)
-        } else {
-          fail
-        }
-      }
-    case _ => fail
-  }
-
   /** Add a new strategy for merging operators of two types.
     *
     * An example method to merge Filter operators is
@@ -538,7 +299,7 @@ object Rewriter extends LazyLogging {
    * @return
    */
   //noinspection ScalaDocMissingParameterDescription
-  private def removalStrategy(rem: PigOperator): Strategy = {
+  def removalStrategy(rem: PigOperator): Strategy = {
     strategyf((op: Any) => {
       if (op == rem) {
         val pigOp = op.asInstanceOf[PigOperator]
@@ -795,14 +556,5 @@ object Rewriter extends LazyLogging {
     newPlan
   }
 
-  merge[Filter, Filter](mergeFilters)
-  merge[PigOperator, Empty](mergeWithEmpty)
-  merge[RDFLoad, BGPFilter](R2)
-  reorder[OrderBy, Filter]
-  addStrategy(buildBinaryPigOperatorStrategy(filterBeforeJoin))
-  addStrategy(strategyf(t => splitIntoToFilters(t)))
-  addStrategy(removeNonStorageSinks _)
-  addStrategy(R1 _)
-  addStrategy(L2 _)
-  addStrategy(F1)
+  registerAllRules
 }
