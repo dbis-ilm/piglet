d117b6f5bfc3fc8efcb2b27a46a4e9c6fa0ea141
ksattler <kai@Kai-Uwes-MacBook-Air.local>
2015-09-20 11:51:28 +0200

    fix for issue #39
    (cherry picked from commit 346125729f4bb178194b3c52c55fdbbf16eb6131)

--- src/main/scala/dbis/pig/parser/PigParser.scala
+++ src/main/scala/dbis/pig/parser/PigParser.scala
@@ -526,7 +526,7 @@ class PigParser extends JavaTokenParsers with LazyLogging {
   }
 
   /*
-   * MATERIALIE <A>
+   * MATERIALIZE <A>
    */
   def materializeStmt: Parser[PigOperator] = materializeKeyword ~ bag ^^ { case _ ~ b => Materialize(Pipe(b))}
 
