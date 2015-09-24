2628c18b397ad3c9fb7ec45e7c8b4af7a905c7db
Kai-Uwe Sattler <kus@tu-ilmenau.de>
2015-09-24 10:24:54 +0200

    logger added

--- zeppelin/src/main/java/dbis/piglet/PigletInterpreter.java
+++ zeppelin/src/main/java/dbis/piglet/PigletInterpreter.java
@@ -1,35 +1,46 @@
 package dbis.piglet;
 
 import org.apache.zeppelin.interpreter.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.*;
 
 public class PigletInterpreter extends Interpreter {
-	public PigletInterpreter(Properties property) {
+    Logger logger = LoggerFactory.getLogger(PigletInterpreter.class);
+
+    static {
+        Interpreter.register("piglet", PigletInterpreter.class.getName());
+    }
+
+    public PigletInterpreter(Properties property) {
         super(property);
-	}
+        logger.info("PigletInterpreter created");
+    }
 
-	public void open() {
-	}
+    public void open() {
+        logger.info("PigletInterpreter.open");
+    }
 
-	public void close() {
-	}
+    public void close() {
+        logger.info("PigletInterpreter.close");
+    }
 
-	public void cancel(InterpreterContext context) {
-	}
+    public void cancel(InterpreterContext context) {
+    }
 
     public FormType getFormType() {
         return FormType.SIMPLE;
     }
 
-	public int getProgress(InterpreterContext context) {
-		return 100;
-	}
+    public int getProgress(InterpreterContext context) {
+        return 100;
+    }
 
-	public List<String> completion(String buf, int cursor) {
+    public List<String> completion(String buf, int cursor) {
         return new ArrayList<String>();
-	}
+    }
 
-	public InterpreterResult interpret(String st, InterpreterContext context) {
+    public InterpreterResult interpret(String st, InterpreterContext context) {
         return null;
-	}
+    }
 }
