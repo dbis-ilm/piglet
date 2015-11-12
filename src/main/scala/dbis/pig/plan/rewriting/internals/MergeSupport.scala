aca97e43629fa3007eab74917a998851994193bd
Stefan Hagedorn <stefan.hagedorn@tu-ilmenau.de>
2015-11-12 14:56:32 +0100

    works - but not for Joins

--- src/main/scala/dbis/pig/plan/rewriting/internals/MergeSupport.scala
+++ src/main/scala/dbis/pig/plan/rewriting/internals/MergeSupport.scala
@@ -1,60 +1,55 @@
 package dbis.pig.plan.rewriting.internals
 
 import dbis.pig.plan.DataflowPlan
-import dbis.pig.tools.DepthFirstTopDownWalker
 import dbis.pig.op.PigOperator
-import com.typesafe.scalalogging.LazyLogging
-import org.kiama.rewriting.Strategy
-import dbis.pig.plan.DataflowPlan
-import dbis.pig.plan.DataflowPlan
-import dbis.pig.op.PigOperator
+import dbis.pig.tools.BreadthFirstTopDownWalker
+
 import org.kiama.rewriting.Rewriter._
 
+
+import com.typesafe.scalalogging.LazyLogging
 trait MergeSupport extends LazyLogging {
 
-//  def processPlan(newPlan: DataflowPlan, strategy: Strategy): DataflowPlan
-//  
-//  def mergePlans2(schedule: Seq[DataflowPlan]): DataflowPlan = {
-//    
-//    var mergedPlan = schedule.head
-//    
-//    val strategy = (op: Any) => {
-//      val opOp = op.asInstanceOf[PigOperator]
-//      if(!mergedPlan.containsOperator(opOp)) {
-//        opOp.inputs.map { p => p.producer }.foreach { prod => mergedPlan.insertAfter(prod, opOp) }
-//        Some(opOp)
-//      } else
-//        None
-//    }
-//    
-//    // TODO: how to process multiple plans here?
-//    processPlan(mergedPlan, strategyf(t => strategy(t)))
-//  }
   
 	def mergePlans(schedule: Seq[DataflowPlan]): DataflowPlan = {
+	  
+	  schedule.zipWithIndex.foreach { case (plan,idx) =>  
+	    plan.operators.foreach { op => op.outputs.foreach { pipe => pipe.name += s"_$idx" } }  
+	    
+	    plan.printPlan()
+	  }
+	  
+	  
 		var mergedPlan = schedule.head
 
-		val walker = new DepthFirstTopDownWalker
+		val walker = new BreadthFirstTopDownWalker
 
 		/* the visitor to process all operators and add them to the merged
 		 * plan if necessary
 		 */
 		def visitor(op: PigOperator): Unit = {
 	    logger.debug(s"processing op: $op")
-			if(!mergedPlan.containsOperator(op)) {
+	    
+	    val mergedOps = mergedPlan.findOperator { o => o.lineageSignature == op.lineageSignature} 
+	    
+	    logger.debug(s"For $op --> Ops in merged plan: $mergedOps")
+	    
+			if(mergedOps.isEmpty) {
 			  logger.debug(s"$op not already part of merged plan")
 			  
 			  op.inputs.map { pipe => pipe.producer.lineageSignature }
 			    .flatMap { lineage => mergedPlan.findOperator { o => o.lineageSignature == lineage } }
 			    .foreach { producer =>
+			      
+			      logger.debug(s"PRODUCER: $producer")
+			      
+			      // FIXME: for SPLIT and JOIN we have to find the correct pipe
+			      op.inputs.head.name = producer.outPipeName
+			      
   					mergedPlan = mergedPlan.insertAfter(producer, op)
   					logger.debug(s"inserted $op after $producer")
 				}
 			  
-//				op.inputs.map { pipe => pipe.producer }.foreach { producer =>
-//					mergedPlan = mergedPlan.insertAfter(producer, op)
-//					logger.debug(s"inserted $op after $producer")
-//				}
 			} else {
 			  logger.debug(s"$op already present in plan")
 			}
@@ -64,8 +59,8 @@ trait MergeSupport extends LazyLogging {
 		  .foreach { plan => walker.walk(plan)(visitor) }  // for all remaining: visit each op and add to merged plan    
 
 		
-//		println("merged plan:")
-//		mergedPlan.printPlan(2)
+		println("merged plan:")
+		mergedPlan.printPlan(2)
 		
 		// return the merged plan
 		mergedPlan
