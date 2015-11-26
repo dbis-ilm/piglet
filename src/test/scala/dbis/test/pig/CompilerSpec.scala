ec146fd458523f068172db198b4ed9b611a78dd4
Stefan Hagedorn <stefan.hagedorn@tu-ilmenau.de>
2015-11-26 09:10:49 +0100

    fix compiler error introduced by 0f17150

--- src/test/scala/dbis/test/pig/CompilerSpec.scala
+++ src/test/scala/dbis/test/pig/CompilerSpec.scala
@@ -21,7 +21,8 @@ class CompilerSpec extends FlatSpec with Matchers {
   it should "resolve IMPORT statements recursively" in {
     val source = List("IMPORT 'src/it/resources/import1.pig';", "C = FOREACH B GENERATE $0;")
     val output = PigletCompiler.resolveImports(source.toIterator)
-    output.mkString("\n") should be (
+    val lines = output._1
+    lines.mkString("\n") should be (
       """A = LOAD 'input';
         |B = FILTER A BY $0 > 10;
         |C = FOREACH B GENERATE $0;""".stripMargin)
