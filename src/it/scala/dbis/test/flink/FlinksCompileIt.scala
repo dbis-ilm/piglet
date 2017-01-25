fb92bce05c21bf21322ba38b232ec58307b1b2ae
Kai-Uwe Sattler <kus@tu-ilmenau.de>
2017-01-25 08:02:02 +0100

    integration tests modified

--- src/it/scala/dbis/test/flink/FlinksCompileIt.scala
+++ src/it/scala/dbis/test/flink/FlinksCompileIt.scala
@@ -22,24 +22,26 @@ import org.scalatest.FlatSpec
 
 class FlinksCompileIt extends FlatSpec with CompileIt {
   val scripts = Table(
-    ("script", "result", "truth", "inOrder", "language", "backend"), // only the header of the table
-    ("stream_load.pig",  "result1.out",   "truth/result1.data",   false, "streaming", "flinks"),
-    ("stream_load2.pig", "result2.out",   "truth/result2.data",   false, "streaming", "flinks"),
-    ("stream_foreach1.pig", "distances.out", "truth/distances.data", false, "streaming", "flinks"),
-    ("stream_filter.pig", "filtered.out",  "truth/filtered.data",  false, "streaming", "flinks"),
+    ("script", "result", "truth", "inOrder", "backend"), // only the header of the table
+    ("stream_load.pig",  "result1.out",   "truth/result1.data",   false, "flinks"),
+    ("stream_load2.pig", "result2.out",   "truth/result2.data",   false, "flinks"),
+    ("stream_foreach1.pig", "distances.out", "truth/distances.data", false, "flinks"),
+    ("stream_filter.pig", "filtered.out",  "truth/filtered.data",  false, "flinks"),
 //    ("construct.pig",     "result3.out", "truth/construct.data",   false, "streaming", "flinks"),
 //    ("union.pig",         "united.out",    "truth/united.data",    false, "streaming", "flinks"),
-    ("streaming/aggregate.pig",     "aggregate.out", "truth/aggregate2.data",false, "streaming", "flinks"),
-    ("splitInto.pig",     "splitX.out",    "truth/splitX.data",    false, "streaming", "flinks"),
-    ("windowJoin.pig",    "joinedW.out",   "truth/joined.data",    false, "streaming", "flinks"),
-    ("windowCross.pig",   "crossedW.out",  "truth/crossed.data",   false, "streaming", "flinks"),
-    ("windowGrouping.pig","grouping.out",  "truth/grouping.data",  false, "streaming", "flinks"),
-    ("windowNforeach.pig","nested.out",    "truth/nested.data",    false, "streaming", "flinks"),
-    ("windowFilter.pig",  "filtered.out",  "truth/filtered.data",  false, "streaming", "flinks"),
-    ("windowCount.pig",   "marycounts.out","truth/marycount.data", false, "streaming", "flinks"),
-    ("windowDistinct.pig","unique.out",    "truth/unique.data",    false, "streaming", "flinks"),
-    ("windowSort.pig",    "sorted.out",    "truth/sorted.data",    true, "streaming", "flinks")
+    ("streaming/aggregate.pig",     "aggregate.out", "truth/aggregate2.data",false, "flinks"),
+    ("splitInto.pig",     "splitX.out",    "truth/splitX.data",    false, "flinks"),
+    ("windowJoin.pig",    "joinedW.out",   "truth/joined.data",    false,  "flinks"),
+    ("windowCross.pig",   "crossedW.out",  "truth/crossed.data",   false, "flinks"),
+    ("windowGrouping.pig","grouping.out",  "truth/grouping.data",  false, "flinks"),
+    ("windowNforeach.pig","nested.out",    "truth/nested.data",    false,  "flinks"),
+    ("windowFilter.pig",  "filtered.out",  "truth/filtered.data",  false,  "flinks"),
+    ("windowCount.pig",   "marycounts.out","truth/marycount.data", false,  "flinks"),
+    ("windowDistinct.pig","unique.out",    "truth/unique.data",    false, "flinks"),
+    ("windowSort.pig",    "sorted.out",    "truth/sorted.data",    true, "flinks")
     //TODO: Sample, Split, Stream-Through, Hybrid-Operators
- ) 
- it should behave like checkMatch(scripts)
+ )
+  scripts.zipWithIndex foreach { case ((script, result, truth, inOrder, backend), i) =>
+    checkMatch(script, result, truth, inOrder, backend, i, scripts.size) }
+
 }
