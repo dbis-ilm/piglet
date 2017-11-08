5a30c28c385dbf40fc2dac5e841c254f25caf45b
Stefan Hagedorn <stefan.hagedorn@tu-ilmenau.de>
2017-11-08 21:21:40 +0100

    fix missing impl for string contains

--- sparklib/src/main/scala/dbis/piglet/backends/spark/PigFuncs.scala
+++ sparklib/src/main/scala/dbis/piglet/backends/spark/PigFuncs.scala
@@ -44,7 +44,7 @@ object PigFuncs {
   def uppercase(s: String) = s.toUpperCase
   def lowercase(s: String) = s.toLowerCase
   def concat(s1: String, s2: String) = s1 + s2
-  def contains(s1: String, s2: String) = s1.
+  def contains(s1: String, s2: String) = s1.contains(s2)
   /*
    * Incremental versions of the aggregate functions - used for implementing ACCUMULATE.
    */
