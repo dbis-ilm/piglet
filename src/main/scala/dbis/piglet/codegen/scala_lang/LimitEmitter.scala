f3212016f4e36c18693e2bec114a8c1cb997dc0c
Stefan Hagedorn <stefan.hagedorn@tu-ilmenau.de>
2017-12-20 11:11:12 +0100

    faster impl for limit for small limit-number

--- src/main/scala/dbis/piglet/codegen/scala_lang/LimitEmitter.scala
+++ src/main/scala/dbis/piglet/codegen/scala_lang/LimitEmitter.scala
@@ -17,9 +17,29 @@ class LimitEmitter extends CodeEmitter[Limit] {
       |  res
       |}""".stripMargin
 
+  val smallLimitTemplate = s"""val <out> = sc.parallelize(<in>.take(<num>))<if (profiling)>.map{e =>
+    |  PerfMonitor.sampleSize(e,"<lineage>", accum, randFactor)
+    |  e
+    |}<endif>""".stripMargin
 
-  override def code(ctx: CodeGenContext, op: Limit): String = 
-        render(Map("out" -> op.outPipeName, "in" -> op.inPipeName, "num" -> op.num,"lineage" -> op.lineageSignature))
+  override def code(ctx: CodeGenContext, op: Limit): String = {
+
+    val params = Map(
+      "out" -> op.outPipeName,
+      "in" -> op.inPipeName,
+      "num" -> op.num,
+      "lineage" -> op.lineageSignature)
+
+    if(op.num <= 1000)
+      CodeEmitter.render(smallLimitTemplate,params)
+    else
+      render(params)
+
+  }
+
+
+
+
 
 }
 
