3016337cc93046929d45b5b1a007ea5cde099b93
Stefan Hagedorn <stefan.hagedorn@tu-ilmenau.de>
2018-01-23 09:28:22 +0100

    better code generation for spatial join + index
    produced code for spatial join now creates partioners to speed up the join.
    spatial_index now also adds a spatial partitioning (implicitly)

--- src/main/scala/dbis/piglet/codegen/spark/SpatialIndexEmitter.scala
+++ src/main/scala/dbis/piglet/codegen/spark/SpatialIndexEmitter.scala
@@ -1,22 +1,25 @@
 package dbis.piglet.codegen.spark
 
-import dbis.piglet.codegen.CodeEmitter
-import dbis.piglet.codegen.CodeGenContext
-import dbis.piglet.codegen.scala_lang.ScalaEmitter
-import dbis.piglet.op.IndexOp
-import dbis.piglet.op.IndexMethod
-import dbis.piglet.expr.NamedField
-import dbis.piglet.expr.PositionalField
+import dbis.piglet.codegen.{CodeEmitter, CodeGenContext}
+import dbis.piglet.op.{IndexMethod, IndexOp}
 
 class SpatialIndexEmitter extends CodeEmitter[IndexOp] {
-  override def template = "val <out> = <in><keyby>.index(<params>)"
+//  new dbis.stark.spatial.partitioner.SpatialGridPartitioner(<in>KeyBy, partitionsPerDimension=20, pointsOnly=false)
+  override def template =
+    """val <in>KeyBy = <in><keyby>
+      |val idxParti<out> = new dbis.stark.spatial.partitioner.BSPartitioner(<in>KeyBy, 1, 1000, false)
+      |val <out> = <in>KeyBy.index(Some(idxParti<out>)<if (params)>, <params><endif>)<if (profiling)>.map{ idx =>
+      |  PerfMonitor.sampleSize(idx, "<lineage>", accum, randFactor)
+      |  idx
+      |}<endif>""".stripMargin
   
   override def code(ctx: CodeGenContext, op: IndexOp): String = render(Map(
       "out" -> op.outPipeName,
       "in" -> op.inPipeName,
       "method" -> IndexMethod.methodName(op.method),
       "params" -> op.params.mkString(","),
-      "keyby" -> SpatialEmitterHelper.keyByCode(op.inputSchema, op.field, ctx)
+      "keyby" -> SpatialEmitterHelper.keyByCode(op.inputSchema, op.field, ctx),
+      "lineage" -> op.lineageSignature
     ) )
 }
 
